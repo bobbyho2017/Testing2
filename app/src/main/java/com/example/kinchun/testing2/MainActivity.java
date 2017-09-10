7e8dfcc61c02e9780d6460800c6fe17713e88ea8
bobbyho2017 <bobbyho2008@gmail.com>
2017-09-10 18:16:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kinchun/testing2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kinchun.testing2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //bobbyho
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
